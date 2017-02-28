f6ff6215c3953fec777eb1918759c1b1b88e4c82
Jacob Taylor <orangewinds@gmail.com>
2017-02-27 17:34:57 -0800

    minimum viable product version

--- weather.class.php
+++ weather.class.php
@@ -9,7 +9,42 @@ class WEATHER
 
     public function getUserByPhone($phone)
     {
-        return $this->database->run('SELECT * FROM users WHERE phone = ?', $phone);
+        return $this->database->run('SELECT * FROM `users` WHERE phone = ?', $phone);
+    }
+    public function checkUserByPhone($phone)
+    {
+        $pdo = $this->database->getPdo();
+        $pdoQuery = $pdo->prepare('SELECT * FROM `users` WHERE phone = :phone');
+            $pdoQuery->bindParam(':phone', $phone);
+            $pdoQuery->execute();
+            $count = $pdoQuery->rowCount();
+            return $count;
+    }
+    public function makeNewUser($phone)
+    {
+        $columns = [
+            'phone',
+            'active'
+        ];
+        $data_array = [
+            $phone,
+            '0'
+        ];
+        $insert = $this->$database->buildInsertQuery('users', $columns);
+        $this->$database->q(
+            $insert,
+            $data_array,
+            \PDO::FETCH_BOTH,
+            true
+        );
+    }
+    public function addZip($phone, $zip)
+    {
+        $pdo = $this->database->getPdo();
+        $pdoQuery = $pdo->prepare('UPDATE `users` SET zip = :zip WHERE phone = :phone');
+            $pdoQuery->bindParam(':zip', $zip);
+            $pdoQuery->bindParam(':phone', $phone);
+            $pdoQuery->execute();
     }
 
 }
--- weatherByText.php
+++ weatherByText.php
@@ -1,20 +1,58 @@
 <?php
-// make twilio/sdk and paragonie/easydb available 
+// make twilio/sdk and paragonie/easydb available
 require_once "vendor/autoload.php";
 // make $database available
 require_once "../database.inc.php";
 // make $sid and $token available
 require_once "../twilio.inc.php";
 require_once "weather.class.php";
-
-use Twilio\Twiml;
-
 $weather = new WEATHER($database);
 
+use Twilio\Twiml;
 $response = new Twiml;
-$response->say("Hello World!");
+use Twilio\Rest\Client;
+$client = new Client($sid, $token);
 
-header("content-type: text/xml");
-echo $response;
+$text_body = trim($_REQUEST['Body']);
+$twilio_service_number = trim($_REQUEST['To']);
+$twilio_client_number = trim($_REQUEST['From']);
+$twilio_message_id = trim($_REQUEST['MessageSid']);
+$twilio_account_id = trim($_REQUEST['AccountSid']);
+$twilio_zip_code_guess = trim($_REQUEST['FromZip']);
+
+/*
+wanted features: user signup with once-yearly subscription fee of $12.
+trigger-able alerts: high above temp or low below temp. trigger on rain chance above 25%.
+alerts in morning or afternoon or both; allow user configuration of when morning/afternoon are.
+*/
+$userCount = $weather->checkUserByPhone($twilio_client_number);
+
+//if ($text_body == 'SIGNUP') {
+switch($userCount) {
+	case 0:
+		$weather->makeNewUser($twilio_client_number);
+		$response->sms('What zip code do you want to receive weather for?');
+	case 1:
+		$user = $weather->getUserByPhone($twilio_client_number);
+		if ($user['active'] == 1) {
+			$response->sms('You already have an active account!');
+		} else {
+			if (is_numeric($body))
+			{
+				$weather->addZip($twilio_client_number, $body);
+				$response->sms('Weather by text now active for '.$body);
+			}
+		}
+	default:
+		// error error problem problem
+}
+//}
+
+if ($text_body == 'STOP') {
+	// do something smart that nukes their account from our system, stops any payments, and sends them a `sorry to see you go` message, not necessarily in that order.
+}
+if ($text_body == 'START') {
+	// re-enable their account. figure out how to make them continue to pay.
+}
 
 ?>
